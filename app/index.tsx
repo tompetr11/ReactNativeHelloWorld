1de22d140b604cecebee389530874580473169f8
tommaso.petrantoni@edu.itspiemonte.it <tommaso.petrantoni@edu.itspiemonte.it>
2024-09-10 14:53:24 +0200

    lesson3

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,11 @@
 import { Image, ScrollView, Text, TextInput, View } from "react-native";
 export default function Index() {
   return (
-  
+    <View style = ({flex:1,justifyContent:'center, alignItems:'center'})>
+  <View style = ({flex : 1 , backgroundcolor: 'red'})>
+  <View style=({flex : 1 , flexDirection:'row'})>
+  <View style=({widht : 100, height: 100, background: })>
+  <View style=({width:150,heigt:100})
+  </View>
   );
 }
